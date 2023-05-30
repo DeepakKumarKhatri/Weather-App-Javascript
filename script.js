cc46f2ecb0f4ce2d19520669c6e5af11dd02b05e
DeepakKumarKhatri <SP21-BCS-017@ISBSTUDENT.COMSATS.EDU.PK>
2023-05-31 01:14:16 +0500

    Completed Logic of Basic Weather App

---
+++ script.js
@@ -0,0 +1,37 @@
+const apiKey = "65f192b8f114321f67573326a04f85e6";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchButton = document.querySelector(".search button");
+const weather_icon = document.querySelector(".weather-icon");
+
+
+async function checkWeather(cityName){
+    const response = await fetch(apiUrl + cityName + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    // update the background images
+    if(data.weather[0].main == "Clouds"){
+        weather_icon.src = "/assests/clouds.png";
+    }else if(data.weather[0].main == "Clear"){
+        weather_icon.src = "/assests/clear.png";
+    }else if(data.weather[0].main == "Rain"){
+        weather_icon.src = "/assests/rain.png";
+    }else if(data.weather[0].main == "Drizzle"){
+        weather_icon.src = "/assests/drizzle.png";
+    }else if(data.weather[0].main == "Mist"){
+        weather_icon.src = "/assests/mist.png";
+    }
+
+    document.querySelector(".weather").style.display = "block";
+
+}
+
+searchButton.addEventListener("click", ()=>{
+    const cityNameValue = searchBox.value;
+    checkWeather(cityNameValue);
+})
