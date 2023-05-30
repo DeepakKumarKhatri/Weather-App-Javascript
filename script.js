31331c44d92cd61a1026f775b50ad415bfc1f03a
DeepakKumarKhatri <SP21-BCS-017@ISBSTUDENT.COMSATS.EDU.PK>
2023-05-31 01:40:41 +0500

    Handled issues of empty strings

--- script.js
+++ script.js
@@ -8,35 +8,49 @@ const weather_icon = document.querySelector(".weather-icon");
 async function checkWeather(cityName){
     const response = await fetch(apiUrl + cityName + `&appid=${apiKey}`);
     
-    if(response.status == 404){
-        document.querySelector(".error").style.display = "block";
-        document.querySelector(".weather").style.display = "none";
-    }else{
-
-        var data = await response.json();
-
-        document.querySelector(".city").innerHTML = data.name;
-        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
-
-        // update the background images
-        if(data.weather[0].main == "Clouds"){
-            weather_icon.src = "/assests/clouds.png";
-        }else if(data.weather[0].main == "Clear"){
-            weather_icon.src = "/assests/clear.png";
-        }else if(data.weather[0].main == "Rain"){
-            weather_icon.src = "/assests/rain.png";
-        }else if(data.weather[0].main == "Drizzle"){
-            weather_icon.src = "/assests/drizzle.png";
-        }else if(data.weather[0].main == "Mist"){
-            weather_icon.src = "/assests/mist.png";
+    if(searchBox.value !== ""){
+        if(response.status == 404){
+            document.querySelector(".city-error").style.display = "block";
+            document.querySelector(".weather").style.display = "none";
+        }else{
+    
+            var data = await response.json();
+    
+            document.querySelector(".city").innerHTML = data.name;
+            document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+            document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+            document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    
+            // update the background images
+            if(data.weather[0].main == "Clouds"){
+                weather_icon.src = "/assests/clouds.png";
+            }else if(data.weather[0].main == "Clear"){
+                weather_icon.src = "/assests/clear.png";
+            }else if(data.weather[0].main == "Rain"){
+                weather_icon.src = "/assests/rain.png";
+            }else if(data.weather[0].main == "Drizzle"){
+                weather_icon.src = "/assests/drizzle.png";
+            }else if(data.weather[0].main == "Mist"){
+                weather_icon.src = "/assests/mist.png";
+            }
+    
+            document.querySelector(".weather").style.display = "block";
+            document.querySelector(".city-error").style.display = "none";
         }
-
-        document.querySelector(".weather").style.display = "block";
-        document.querySelector(".error").style.display = "none";
+    }else{
+        document.querySelector(".empty-string-error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
     }
+
+    
 }
+
+searchBox.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.key === "Enter") {
+        searchButton.click();
+    }
+});
 
 searchButton.addEventListener("click", ()=>{
     const cityNameValue = searchBox.value;
