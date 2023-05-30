65480d871d8a1a92a13a71410fd2c38a943425fd
DeepakKumarKhatri <SP21-BCS-017@ISBSTUDENT.COMSATS.EDU.PK>
2023-05-31 01:19:33 +0500

    Handled issues of city misspelling

--- script.js
+++ script.js
@@ -7,28 +7,35 @@ const weather_icon = document.querySelector(".weather-icon");
 
 async function checkWeather(cityName){
     const response = await fetch(apiUrl + cityName + `&appid=${apiKey}`);
-    var data = await response.json();
+    
+    if(response.status == 404){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }else{
 
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+        var data = await response.json();
 
-    // update the background images
-    if(data.weather[0].main == "Clouds"){
-        weather_icon.src = "/assests/clouds.png";
-    }else if(data.weather[0].main == "Clear"){
-        weather_icon.src = "/assests/clear.png";
-    }else if(data.weather[0].main == "Rain"){
-        weather_icon.src = "/assests/rain.png";
-    }else if(data.weather[0].main == "Drizzle"){
-        weather_icon.src = "/assests/drizzle.png";
-    }else if(data.weather[0].main == "Mist"){
-        weather_icon.src = "/assests/mist.png";
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+        // update the background images
+        if(data.weather[0].main == "Clouds"){
+            weather_icon.src = "/assests/clouds.png";
+        }else if(data.weather[0].main == "Clear"){
+            weather_icon.src = "/assests/clear.png";
+        }else if(data.weather[0].main == "Rain"){
+            weather_icon.src = "/assests/rain.png";
+        }else if(data.weather[0].main == "Drizzle"){
+            weather_icon.src = "/assests/drizzle.png";
+        }else if(data.weather[0].main == "Mist"){
+            weather_icon.src = "/assests/mist.png";
+        }
+
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
     }
-
-    document.querySelector(".weather").style.display = "block";
-
 }
 
 searchButton.addEventListener("click", ()=>{
